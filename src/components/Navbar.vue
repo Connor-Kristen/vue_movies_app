4b9e39c89078048fb8ef9ed4743ee4f686efa7e8
Connor Wulf <connor.m.wulf@gmail.com>
2021-01-25 08:53:24 -0600

    adds tailwind

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <router-link :to="{name: 'Home'}">HOME</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
