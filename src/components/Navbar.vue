47cd835726d0e7029d2b2598b74306835dc1faf5
Connor Wulf <connor.m.wulf@gmail.com>
2021-01-25 16:51:30 -0600

    adds basic navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,9 @@
 <template>
-  <nav>
-    <router-link :to="{name: 'Home'}">HOME</router-link>
+  <nav class="p-2 bg-green-300">
+    <div class="flex justify-center">
+      <h1 class="flex-grow">NOTFLIX</h1>
+      <router-link :to="{name: 'Home'}">HOME</router-link>
+    </div>
   </nav>
 </template>
 
@@ -9,7 +12,3 @@ export default {
   name: "Navbar"
 }
 </script>
-
-<style scoped>
-
-</style>
