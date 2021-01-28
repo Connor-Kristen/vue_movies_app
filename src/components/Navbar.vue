90ffd9ce20f37bb365638cfe165fd775ac536878
Connor Wulf <connor.m.wulf@gmail.com>
2021-01-27 18:07:34 -0600

    fetches movies from json server

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,28 +1,30 @@
 <template>
-  <nav class="nav bg-black bg-opacity-40 overflow-y-hidden"
-       :class="{expand: navExpand, collapse: !navExpand}">
-    <div class="max-w-4xl mx-auto p-2 flex flex-col">
-      <div class="flex items-center justify-center">
-        <div class="flex-grow flex items-center">
-          <span class="material-icons text-gray-300">movie</span>
-          <h1 class="text-green-300 text-lg inline-block">NOTFLIX</h1>
+  <teleport to="#navbar">
+    <div class="nav bg-black bg-opacity-40 overflow-y-hidden"
+         :class="{expand: navExpand, collapse: !navExpand}">
+      <div class="max-w-4xl mx-auto p-2 flex flex-col sm:flex-row sm:justify-between">
+        <div class="flex items-center justify-center">
+          <div class="flex-grow flex items-center">
+            <span class="material-icons text-gray-300">movie</span>
+            <h1 class="text-green-300 text-lg inline-block">NOTFLIX</h1>
+          </div>
+          <div ref="barContainer" class="w-9 h-7 inline-block self-end sm:hidden bar-container" @click="expandNavBar">
+            <span ref="bar" class="bar "></span>
+          </div>
         </div>
-        <div class="w-9 h-7 inline-block self-end bar-container" @click="expandNavBar">
-          <span class="bar"></span>
+        <div class="flex mx-2 mt-6 flex-col sm:flex-row sm:mt-0">
+          <router-link :to="{name: 'Home'}" class="flex text-sm text-gray-500 items-center">
+            <span class="material-icons text-base mr-0.5">home</span>
+            HOME
+          </router-link>
+          <router-link :to="{name: 'Home'}" class="flex text-sm text-gray-500 items-center">
+            <span class="material-icons text-base d mr-0.5">add_circle</span>
+            ADD MOVIE
+          </router-link>
         </div>
-      </div>
-      <div class="flex mx-2 mt-6 flex-col">
-        <router-link :to="{name: 'Home'}" class="flex text-sm text-gray-500 items-center">
-          <span class="material-icons text-base mr-0.5">home</span>
-          HOME
-        </router-link>
-        <router-link :to="{name: 'Home'}" class="flex text-sm text-gray-500 items-center">
-          <span class="material-icons text-base d mr-0.5">add_circle</span>
-          ADD MOVIE
-        </router-link>
       </div>
     </div>
-  </nav>
+  </teleport>
 </template>
 
 <script>
@@ -40,6 +42,8 @@ export default {
   methods: {
     expandNavBar () {
       this.navExpand = !this.navExpand;
+      this.$refs.barContainer.classList.toggle("active");
+      this.$refs.bar.classList.toggle("bar-active");
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,30 @@
 <template>
   <Navbar/>
+  <div v-if="movies.length > 0" class="relative">
+      <SingleMovie v-for="(movie, i) in movies" :key="movie.id" :movie="movie" :index="i"/>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from '@/components/Navbar';
+import SingleMovie from "@/components/SingleMovie";
 export default {
   name: 'Home',
   components: {
-    Navbar
+    Navbar,
+    SingleMovie
+  },
+  data () {
+    return {
+      movies: []
+    }
+  },
+  mounted () {
+   return fetch(" http://localhost:3000/movies")
+    .then(res => res.json())
+    .then(data => this.movies = data)
+    .catch(console.log)
   }
 }
 </script>
