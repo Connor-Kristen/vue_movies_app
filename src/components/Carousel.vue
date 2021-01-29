96a1e481b8fabfb9e34da74a9dde6041d2843972
Connor Wulf <connor.m.wulf@gmail.com>
2021-01-28 18:52:21 -0600

    adds basic carousel layout

---
+++ src/components/Carousel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="relative h-72">
+    <slot></slot>
+    <button class="h-full block absolute w-9 bg-black bg-opacity-50 rounded-xl">
+      <span class="material-icons text-4xl text-gray-400">chevron_left</span>
+    </button>
+    <button class="h-full block absolute w-9 bg-black bg-opacity-50 right-0 rounded-xl">
+      <span class="material-icons text-4xl text-gray-400 ">chevron_right</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Carousel",
+  props: ["imgs"],
+  mounted () {
+    console.log(this.imgs);
+  }
+}
+</script>
--- src/components/SingleMovie.vue
+++ src/components/SingleMovie.vue
@@ -1,18 +1,16 @@
 <template>
-    <img ref="poster" :src="movie.poster" :alt="'poster for '+movie.title" class="poster inline-block w-44 h-72 object-cover absolute">
+<!--    <img ref="poster" :src="movie.poster" :alt="'poster for '+movie.title" class="poster inline-block w-48 h-72 object-cover absolute">-->
+  <img ref="poster" src="../assets/logo.png" alt="" class="poster inline-block w-48 h-full object-cover absolute">
 </template>
 
 <script>
 export default {
   name: "SingleMovie",
   props: ["movie", "index"],
-  data() {
-    return {
-      left: 0
-    }
-  },
   mounted () {
-    console.log(this.index);
+    let left = this.index * this.$refs.poster.offsetWidth
+    if (this.index > 0) left += (5 * this.index)
+    this.$refs.poster.style.left = left + "px"
   }
 }
 </script>
