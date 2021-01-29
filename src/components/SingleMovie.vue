b8f57de0561cde6fdf736cfd0d507552c2f7bf44
Connor Wulf <connor.m.wulf@gmail.com>
2021-01-29 16:33:58 -0600

    adds moving carousel

--- src/components/SingleMovie.vue
+++ src/components/SingleMovie.vue
@@ -1,6 +1,6 @@
 <template>
 <!--    <img ref="poster" :src="movie.poster" :alt="'poster for '+movie.title" class="poster inline-block w-48 h-72 object-cover absolute">-->
-  <img ref="poster" src="../assets/logo.png" alt="" class="poster inline-block w-48 h-full object-cover absolute">
+  <img ref="poster" :style="'background:'+bgColor()" src="../assets/logo.png" alt="" class="poster inline-block w-48 h-full object-cover absolute">
 </template>
 
 <script>
@@ -8,9 +8,17 @@ export default {
   name: "SingleMovie",
   props: ["movie", "index"],
   mounted () {
-    let left = this.index * this.$refs.poster.offsetWidth
+    let left = (this.index * this.$refs.poster.offsetWidth) - (this.$refs.poster.offsetWidth / 3);
     if (this.index > 0) left += (5 * this.index)
     this.$refs.poster.style.left = left + "px"
+  },
+  methods: {
+    bgColor () {
+      let red = Math.ceil(Math.random() * 255)
+      let green =Math.ceil(Math.random() * 255)
+      let blue =Math.ceil(Math.random() * 255)
+      return `rgb(${red}, ${green}, ${blue})`
+    }
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,16 @@
 <template>
   <Navbar/>
-  <carousel v-if="movies.length > 0">
-      <SingleMovie v-for="(movie, i) in 10" :key="movie.id" :movie="movie" :index="i"/>
-  </carousel>
+  <carousel v-if="movies.length > 0" />
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from '@/components/Navbar';
-import SingleMovie from "@/components/SingleMovie";
 import Carousel from "@/components/Carousel";
 export default {
   name: 'Home',
   components: {
     Navbar,
-    SingleMovie,
     Carousel
   },
   data () {
@@ -23,7 +19,7 @@ export default {
     }
   },
   mounted () {
-   return fetch(" http://localhost:3000/movies")
+    fetch(" http://localhost:3000/movies")
     .then(res => res.json())
     .then(data => this.movies = data)
     .catch(console.log)
